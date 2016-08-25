bc2b1e5fff7011dec8c19c4eeaaf9fcda5797b90
sethjgore <sethjgore@gmail.com>
2016-08-25 10:59:29 -0400

    updated z-index issues conflicting with navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+    var $ = jQuery;
 
+    function animateWelcomeBox() {
+        $('#j-welcome').addClass('visible');
+        console.log('animating welcome box');
+    }
+
+    function toggleMobileMenu() {
+        $('#j-navmobilemenu').toggleClass('visible');
+        console.log('toggling Mobile Menu Visiblity');
+    }
+
+    function animateBody() {
+        $('body').addClass('visible');
+        console.log('animating welcome box');
+    }
+
+    function animateWelcomeBox() {
+        $('#j-welcome').addClass('visible');
+        console.log('animating welcome box');
+    }
+
+    //jquery's condensed document.ready format
+    $(function() {
+        console.log('script init');
+        window.setTimeout(animateWelcomeBox, 2000);
+
+        animateBody();
+
+        $('#j-navmobilemenu-open').click(function() {
+            toggleMobileMenu();
+            console.log('clicking open button');
+        });
+
+        $('#j-navmobilemenu-close').click(function() {
+            toggleMobileMenu();
+            console.log('clicking close button');
+        });
+    });
