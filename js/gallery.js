c3a85fa887cce13488ca5aed0b4e053de891b456
sethjgore <sethjgore@gmail.com>
2016-09-08 16:02:11 -0400

    finished with video gallery

---
+++ js/gallery.js
@@ -0,0 +1,56 @@
+    function closeGallery() {
+        $('#j-closeGallery').click(function() {
+            $('#j-gallery-overlay').toggleClass('hidden');
+            new Flickity('#j_library-gallery', {
+                accessibility: true,
+                contain: true,
+                setGallerySize: false,
+                draggable: false,
+                pageDots: false
+            });
+        });
+    }
+
+    function initGalleryFunctions() {
+        var gallery = new Flickity('#j_library-gallery', {
+            accessibility: true,
+            contain: true,
+            setGallerySize: false,
+            draggable: false,
+            pageDots: false
+        });
+
+
+        $('#j-closeGallery').click(function() {
+            $('#j-gallery-overlay').toggleClass('hidden');
+        });
+
+        $('#j-openGallery').click(function() {
+            $('#j-gallery-overlay').toggleClass('hidden');
+        });
+
+        function listener() {
+            console.log('this happened');
+        }
+
+        //autoplays & pauses each video when selected.
+        gallery.on('select', function() {
+
+            //pauses all videos on page
+            $('video').each(function() {
+                $(this).get(0).pause();
+                var ispaused = $(this).get(0).paused;
+                console.log('video is paused: ' + ispaused);
+            });
+
+            //starts only the current video
+            $('.is-selected video').get(0).play(function() {
+                var isplaying = $(this).get(0).playing;
+                console.log('video is playing: ' + isplaying);
+            });
+        })
+    }
+
+    $(function() {
+       initGalleryFunctions();
+    });
--- js/main.js
+++ js/main.js
@@ -10,55 +10,61 @@
         console.log('toggling Mobile Menu Visiblity');
     }
 
-    function animateBody() {
-        $('body').addClass('visible');
-        console.log('animating welcome box');
-    }
-
-    function animateWelcomeBox() {
-        $('#j-welcome').addClass('visible');
-        console.log('animating welcome box');
-    }
-
-    function toggleSubtitles(video){
+    function toggleSubtitles(video) {
         $('#j-welcome').toggleClass('invisible');
-        if(video.textTracks[0].mode == "showing"){
+        if (video.textTracks[0].mode == "showing") {
             video.textTracks[0].mode = "hidden";
-        }else{
+        } else {
             video.textTracks[0].mode = "showing";
         }
     }
 
-    //jquery's condensed document.ready format
+    //runs when the document is ready.
     $(function() {
-        console.log('script init');
+
+        //SETUP: starts the welcome box animation
         window.setTimeout(animateWelcomeBox, 2000);
 
-        animateBody();
+        //SETUP: intro video if it is available on the page
+        if ($('#introvideo').length) {
+            console.log($('#introvideo'));
+            $('#introvideo').get(0).play();
+        }
 
+        //SETUP: hides main videos' subtitles if they are available
+        if ($('#mainvideo').length) {
+            v = $('#mainvideo').get(0);
+            v.textTracks[0].mode = "hidden";
+        }
+
+        //EVENT: click subtitles button (show/hide subtitles)
+        if ($('#subtitles').length) {
+            $('#subtitles').click(function() {
+                toggleSubtitles($('#mainvideo').get(0));
+            })
+        }
+
+        //EVENT: click open menu button on navigation bar
         $('#j-navmobilemenu-open').click(function() {
             toggleMobileMenu();
             console.log('clicking open button');
         });
 
+        //EVENT: click close button on mobile overlay menu
         $('#j-navmobilemenu-close').click(function() {
             toggleMobileMenu();
             console.log('clicking close button');
         });
 
-        if($('#introvideo').length) {
-            console.log($('#introvideo'));
-            $('#introvideo').get(0).play();
-        }
+    });
 
-        if($('#mainvideo').length){
-            v = $('#mainvideo').get(0);
-            v.textTracks[0].mode = "hidden";
-        }
-
-        if($('#subtitles').length){
-            $('#subtitles').click(function(){
-                toggleSubtitles($('#mainvideo').get(0));
-            })
-        }
+    //initalizes the topic video gallery
+    $(function() {
+        $(document).on('click', 'video', function() {
+            if (this.paused) {
+                this.play();
+            } else {
+                this.pause();
+            }
+        });
     });
